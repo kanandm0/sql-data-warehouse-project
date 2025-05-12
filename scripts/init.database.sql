2d6e74bc9f8d76c7ad63defd624363d2f611ef13
kanandm0 <kanandm0@gmail.com>
2025-05-12 11:52:30 +0530

    Create init.database.sql

---
+++ scripts/init.database.sql
@@ -0,0 +1,18 @@
+/*
+SCRIPT PURPOSE:
+
+WARINING:
+
+*/
+
+-- CREATE database 'DataWareHouse'
+
+USE MASTER;
+
+CREATE DATABASE DataWarehouse;
+
+USE DataWarehouse;
+
+CREATE SCHEMA bronze;
+CREATE SCHEMA silver;
+CREATE SCHEMA gold;
